fa3380bc1f95b5a434599bfc31640a555cbf95d5
Anthony Duque <anthony@anthonys-mbp-2.mynetworksettings.com>
2024-07-01 06:21:49 -0400

    updates

--- src/app/page.js
+++ src/app/page.js
@@ -2,22 +2,51 @@ import CSVReader from "../components/CSVReader";
 import Collection from "../components/Collection";
 import { deleteProducts, getProducts } from "../../services/productServices";
 import Button from "@/components/Button";
+import { useEffect, useState } from "react";
+
 
 export default async function Home() {
-  // const products = await getProducts()
+  const [data, setData] = useState([]);
+  const [error, setError] = useState(null);
 
-  // console.log(products)
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch('https://jdw-api-e99ec4f472ac.herokuapp.com/csv-data'); // replace with your Heroku app URL
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        const result = await response.json();
+        setData(result);
+      } catch (error) {
+        setError(error.message);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  if (error) {
+    return <div>Error: {error}</div>;
+  }
 
   return (
     <main>
       <div>
         <span>Welcome Home</span>
       </div>
+
       <div>
-        {/* <Button /> */}
-        {/* <CSVReader /> */}
-        {/* <button onClick={await getProducts()}>Load Items</button> */}
-      </div>
+        <ul>
+          {data.map((item, index) => (
+            <li key={index}>{JSON.stringify(item)}</li>
+          ))}
+        </ul>
+    </div>
+
+      {/* <Button /> */}
+      {/* <CSVReader /> */}
+      {/* <button onClick={await getProducts()}>Load Items</button> */}
       {/* <Collection products={products}/> */}
     </main>
   );
